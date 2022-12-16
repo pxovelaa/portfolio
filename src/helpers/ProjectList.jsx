b17f9a60fa714ef8bd3563f51827e68ce946de3f
pxovelaa <gio.pxovelishvili@gmail.com>
2022-12-16 12:40:30 +0300

    add Website button

--- src/helpers/ProjectList.jsx
+++ src/helpers/ProjectList.jsx
@@ -6,6 +6,7 @@ import Domains from '../assets/Domains.png';
 import Expenses from '../assets/Expenses.png';
 import Project4 from '../assets/Project4.png';
 import Redberry from '../assets/Redberry.png';
+import Business from '../assets/Business.png';
 
 
 export const projectList = [
@@ -13,48 +14,63 @@ export const projectList = [
         name: 'Youtube clone app',
         image: YoutubeCloneApp,
         skills: 'ReactJS, Javascript, MaterialUI, CSS',
-        link: 'https://github.com/pxovelaa/youtube-clone-app'
+        link: 'https://github.com/pxovelaa/youtube-clone-app',
+        website: "https://asyoutube.netlify.app"
+    },
+    {
+        name: 'Business app',
+        image: Business,
+        skills: 'ReactJS, Javascript, Tailwind CSS, CSS',
+        link: 'https://github.com/pxovelaa/modern-bussines-website',
+        website: "quiet-praline-2ff7f1.netlify.app/"
     },
     {
         name: 'Sapatriarqo Website',
         image: Sapatriarqo,
         skills: 'ReactJS, Javascript, MaterialUI, CSS',
-        link: 'https://github.com/pxovelaa/Sapatriarqo'
+        link: 'https://github.com/pxovelaa/Sapatriarqo',
+        website: "https://pxovelaa.github.io/Sapatriarqo/"
     },
     {
         name: 'Marketing Markup Website',
         image: MarketingMarkup,
         skills: 'HTML, CSS',
-        link: 'https://github.com/pxovelaa/Giorgi-Pkhovelishvili'
+        link: 'https://github.com/pxovelaa/Giorgi-Pkhovelishvili',
+        website: "pxovelaa.github.io/Giorgi-Pkhovelishvili/"
     },
     {
         name: 'Acme Website',
         image: Acme,
         skills: 'HTML, CSS',
-        link: 'https://github.com/pxovelaa/ACME-Web-SIte'
+        link: 'https://github.com/pxovelaa/ACME-Web-SIte',
+        website: "pxovelaa.github.io/ACME-Web-SIte/"
     },
     {
         name: 'Domains Website',
         image: Domains,
         skills: 'Javascript, HTML, CSS',
-        link: 'https://github.com/pxovelaa/Domenebis-site'
+        link: 'https://github.com/pxovelaa/Domenebis-site',
+        website: "https://pxovelaa.github.io/Domenebis-site/"
     },
     {
         name: 'Expenses Website',
         image: Expenses,
         skills: 'ReactJS, HTML, CSS',
-        link: 'https://github.com/pxovelaa/React-Expenses'
+        link: 'https://github.com/pxovelaa/React-Expenses',
+        website: "https://github.com/pxovelaa/React-Expenses"
     },
     {
         name: 'React test Project Website',
         image: Project4,
         skills: 'ReactJS, HTML, CSS',
-        link: 'https://github.com/pxovelaa/React-project4'
+        link: 'https://github.com/pxovelaa/React-project4',
+        website: "https://github.com/pxovelaa/React-project4"
     },
     {
         name: 'Redberry Website',
         image: Redberry,
         skills: 'ReactJS, Javascript, HTML, CSS',
-        link: 'https://github.com/pxovelaa/Redberry-Project'
+        link: 'https://github.com/pxovelaa/Redberry-Project',
+        website: "https://github.com/pxovelaa/Redberry-Project"
     }
 ]
--- src/pages/ProjectDisplay.jsx
+++ src/pages/ProjectDisplay.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { projectList } from '../helpers/ProjectList';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import '../styles/ProjectDisplay.css';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { projectList } from "../helpers/ProjectList";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import "../styles/ProjectDisplay.css";
 
 function ProjectDisplay() {
-    const { id } = useParams();
-    const project = projectList[id];
+  const { id } = useParams();
+  const project = projectList[id];
   return (
-    <div className='project'>
-        <h1>{project.name} </h1>
-        <img src={project.image} alt={project.name} />
-        <p>
-            <b>Skills: </b> {project.skills}
-        </p>
-        <a target='_blank' href={project.link}><GitHubIcon /></a>
+    <div className="project">
+      <h1>{project.name} </h1>
+      <img src={project.image} alt={project.name} />
+      <p>
+        <b>Skills: </b> {project.skills}
+      </p>
+      <div className="projectDisplay-buttons">
+        <a target="_blank" href={project.link}>
+          <GitHubIcon />
+        </a>
+        <a target='_blank' href={project.website} className="web-button" > Visit Website</a>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProjectDisplay
+export default ProjectDisplay;
